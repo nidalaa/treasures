eb4923d6efc8c3c12a8dc16cc4ca7fa32e8baec3
Emilia Andrzejewska <alime1722@gmail.com>
2019-02-26 16:08:18 +0100

    Linter errors

--- example/treasure.rb
+++ example/treasure.rb
@@ -5,9 +5,9 @@ class Treasure
   def initialize(owner, type)
     @id = Random.rand(1_000...20_000)
     @owner = owner
-    @type = type
+      @type = type
 
-    # IMPORTANT: now Treasure class makes sure that newly created object is added to global collection
+    ##IMPORTANT: now Treasure class makes sure that newly created object is added to global collection
     TreasuresCollection.add(self)
   end
 end
